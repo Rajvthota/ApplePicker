2eb6ed1d610638fb8aa8712bf46823b8db253135
Raaj Thota <43080673+Rajvthota@users.noreply.github.com>
2018-09-21 15:58:56 -0400

    added apple code
    added apple code and physics

--- Assets/Apple.cs
+++ Assets/Apple.cs
@@ -2,15 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Apple : MonoBehaviour {
+public class Apple : MonoBehaviour
+{
+    public static float bottomY = -20f;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    void Update()
+    {
+        if( transform.position.y < bottomY)
+        {
+            Destroy(this.gameObject);
+        }
+    }
 }
--- Assets/AppleTree.cs
+++ Assets/AppleTree.cs
@@ -9,28 +9,46 @@ public class AppleTree : MonoBehaviour {
 
     public float leftAndRightEdge = 10f;
 
-    public float chanceToChangeDirection = 0.1f;
+    public float chanceToChangeDirections = 0.1f;
 
     public float secondsBetweenAppleDrops = 1f;
 
     void Start()
     {
-        
+        Invoke("DropApple", 2f);
+    }
+
+    void DropApple()
+    {
+        GameObject apple = Instantiate<GameObject>(applePrefab);
+
+        apple.transform.position = transform.position;
+
+        Invoke("DropApple", secondsBetweenAppleDrops);
     }
 
     void Update()
     {
         Vector3 pos = transform.position;
         pos.x += speed * Time.deltaTime;
-        transform.position = pos; 
+        transform.position = pos;
 
         if (pos.x <= leftAndRightEdge)
         {
             speed = Mathf.Abs(speed);
         }
-        else if ( pos.x > leftAndRightEdge)
+        else if (pos.x > leftAndRightEdge)
         {
             speed = -Mathf.Abs(speed);
+        }
+ 
+    }
+
+    void FixedUpdate()
+    {
+    if (Random.value < chanceToChangeDirections)
+        {
+            speed *= -1; 
         }
     }
 
