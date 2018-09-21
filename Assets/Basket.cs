247ffe34fd22f8aa51fbe8a7fba3584f39f12970
Raaj Thota <43080673+Rajvthota@users.noreply.github.com>
2018-09-21 16:21:30 -0400

    Errors Occuring
    Errors in apple picker scripts, I have no idea

--- Assets/Basket.cs
+++ Assets/Basket.cs
@@ -2,15 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript2 : MonoBehaviour {
+public class Basket : MonoBehaviour
+{
+     void Update()
+    {
+        Vector3 mousePos2D = Input.mousePosition;
+        Vector3 mousePos2D.z = -Camera.main.transform.position.z;
+        Vector3 mousePos3D = Camera.main.ScreenToWorldPoint(mousePos2D);
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+        Vector3 pos = this.transform.position;
+        pos.x = mousePos3D.x;
+        this.transform.position = pos; 
+
+    }
 }
